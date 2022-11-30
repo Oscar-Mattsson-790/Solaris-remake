69d5a95db58b5a5a2177121b2c8ee63327c6b2dd
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2022-11-29 19:09:33 -0600

    Ongoing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const BASE_URL = "https://fathomless-shelf-54969.herokuapp.com";
+const pageOne = document.querySelector("#pageOne");
+const pageTwo = document.querySelector("#pageTwo");
+const buttons = document.querySelectorAll("button");
+
+async function getKey() {
+  const response = await fetch(`${BASE_URL}/keys`, { method: "POST" });
+  const data = await response.json();
+  console.log(data);
+
+  return data.key;
+}
+
+async function getPlanets() {
+  const key = await getKey();
+  const response = await fetch(`${BASE_URL}/bodies`, {
+    headers: {
+      "x-zocom": key,
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+}
